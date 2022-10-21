dfd38cfa451ef97fc7162b536ca99fb0304b44da
Richard Abrich <richard.abrich@gmail.com>
2022-10-21 01:36:37 -0400

    form-name feedback -> register; remove comments

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -10,25 +10,13 @@ export default function FeedbackForm() {
         method="POST"
         action="/success"
       >
-        <input type="hidden" name="form-name" value="feedback" />
+        <input type="hidden" name="form-name" value="register" />
         <p className={styles.hidden}>
             <label>
             Don’t fill this out if you’re human: <input name="bot-field" />
             </label>
         </p>
-  
-        {/*
-        <label htmlFor="name">Name</label>
-        <input id="name" className={styles['form-field']} type="text" name="name" />
-
-        <label htmlFor="email">Email</label>
-        */}
         <input id="email" className={styles['form-field']} type="email" name="email" placeholder="your@email.com" required />
-
-        {/*
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
-        */}
         <button className={styles.button} type="submit">Get Monthly Updates</button>
       </form>
   )
