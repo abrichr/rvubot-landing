6dc9d92f175a1c87c832d424e5440f1a75eac8d0
Richard Abrich <richard.abrich@gmail.com>
2022-10-21 11:29:02 -0400

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    <>
+  )
 }
 
 export default Application
