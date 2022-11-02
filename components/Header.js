d9eb989559aa992575bda7395d08e2ece327450e
Richard Abrich <richard.abrich@gmail.com>
2022-11-02 15:32:29 -0500

    add .com to logo

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,13 @@ export default function Header({ title }) {
   return (
     <>
       <div className="flex flex-row justify-center mb-8">
-        <VscHubot className="text-5xl font-bold"/>
-        <div>
-          <h1 className="text-3xl font-bold">{title}</h1>
-          <h4 className="text-l font-bold">1-833-335-6116</h4>
+        <VscHubot className="text-6xl font-bold"/>
+        <div className="text-left ml-2">
+          <div className="flex flex-row mt-1 relative">
+            <h1 className="text-3xl font-bold">{title}</h1>
+            <h4 className="text-xl align-text-bottom absolute bottom-0 -right-8">.com</h4>
+          </div>
+          <h4 className="text-l">1-833-335-6116</h4>
         </div>
       </div>
       <p>
